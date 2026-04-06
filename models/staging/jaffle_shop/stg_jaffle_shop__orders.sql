128e4205c44f2f7a7a75fde48f7e4d61733222db
Paula-GAR <paulagarcia001.alu@stemgranada.com>
2026-04-06 19:40:14 +0200

    Freshness. Data update monitoring.

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,7 +2,8 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+        _etl_loaded_at
 
     -- from raw.jaffle_shop.orders es etático, pero si la DB cambia de raw, deja d funcionar. Por eso mejor así:
 from {{ source('jaffle_shop', 'orders') }}
